aaf3d1c94b34fa8bde0f2b23cb92e5aef9d3dba6
Agacy junior <juniormartinssom@gmail.com>
2022-06-14 20:57:39 -0300

    Header finish e estrutura

---
+++ src/pages/Main.tsx
@@ -0,0 +1,14 @@
+import Frontend from "../templates/Frontend";
+
+import * as S from "./styles";
+
+function Main() {
+  return (
+    <Frontend>
+      <S.Container>
+      </S.Container>
+    </Frontend>
+  );
+}
+
+export default Main;
